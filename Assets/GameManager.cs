4e1949e21356bb77ec187b934b09ffadf138db88
Zach 🦀🌲 <zachary.jay.crabtree@gmail.com>
2018-03-31 12:10:33 +0200

    Refactor into Train Object

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -15,16 +15,9 @@ public class GameManager : MonoBehaviour {
 	public Text originText;
 	public Text destinationText;
 
-	private float velocity = 0;
-	private float acceleration = 0;
-	private float friction = 0;
-	private float frictionCoefficient = .2f;
-	private float jerk = 0;
+	private Train train;
 
-	private float throttleForce = 0;
 	private const float trainMass = 1;
-
-	private float lastAcceleration = 0;
 
 	private DesignedRoutes designedRoutes = new DesignedRoutes();
 
@@ -32,35 +25,24 @@ public class GameManager : MonoBehaviour {
 	private Route currentRoute;
 	private float currentDist;
 
-
 	private StationConnection currentConnection;
 
 	private float timer = 30f;
 
-	void Start () { getNextConnection(); }
+	void Start () {
+		train = createTrain(100f);
+		currentRoute = designedRoutes.u8;
+		getNextConnection();
+		// monitor win/lose condition in game manager
+		// monitor values and output in train object
+
+		// monitor unloading/loading
+	}
 
 	void FixedUpdate () {
+		train.throttleForce = throttle.value * train.getMass();
 
-		throttleForce = throttle.value * trainMass;
-
-		// as velocity increases, friction value approaches acceleration, thus creating an upper limit for velocity
-		if (velocity != 0) {
-			friction = velocity * frictionCoefficient;
-			//friction can only be positive, and rounding to two digits so it can be printed
-			friction = Mathf.Round((friction * 100f)) / 100f;
-		}
-		else {
-			friction = 0;
-		}
-
-		lastAcceleration = acceleration;
-
-		acceleration = throttleForce - friction;
-
-		velocity += acceleration;
-		jerk = (acceleration - lastAcceleration) / Time.fixedDeltaTime;
-
-		currentDist -= velocity;
+		currentDist -= train.getVelocity();
 
 		if (timer > 0) {
 			timer -= Time.deltaTime;
@@ -72,10 +54,11 @@ public class GameManager : MonoBehaviour {
 			getNextConnection();
 		}
 
-		velocityText.text = "Velocity = " + velocity;
-		accelerationText.text = "Acceleration = " + acceleration;
-		jerkText.text = "Jerk = " + jerk;
-		frictionText.text = "Friction = " + friction;
+		// all this needs to be moved to a UI manager
+		velocityText.text = "Velocity = " + train.getVelocity();
+		accelerationText.text = "Acceleration = " + train.getAcceleration();
+		jerkText.text = "Jerk = " + train.getJerk();
+		frictionText.text = "Friction = " + train.getFriction();
 		distanceText.text = "Distance = " + currentDist;
 		timerText.text = "Timer = " + timer;
 		originText.text = "Origin = " + currentConnection.origin.name;
@@ -83,10 +66,23 @@ public class GameManager : MonoBehaviour {
 	}
 
 	private void getNextConnection() {
-		currentRoute = designedRoutes.u8;
-		currentConnection = currentRoute.connections[connectionIterator];
-		currentDist = currentConnection.distance;
-		timer = currentConnection.duration;
-		connectionIterator++;
+		if (currentRoute.connections.Count != connectionIterator) {
+			currentConnection = currentRoute.connections[connectionIterator];
+			currentDist = currentConnection.distance;
+			timer = currentConnection.duration;
+			connectionIterator++;
+		} 
+		else {
+			Debug.Log("You win!");
+		}
+
+	}
+
+	private Train createTrain(float mass) {
+		UnityEngine.Object prefab = Resources.Load("TrainPrefab");
+		GameObject gameObject = Instantiate(prefab) as GameObject;
+		Train trainObject = gameObject.GetComponent<Train>();
+		trainObject.setMass(mass);
+		return trainObject;
 	}
 }
---
+++ Assets/Train.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Train : MonoBehaviour {
+
+	private float mass;
+
+	private float velocity = 0;
+	private float acceleration = 0;
+	private float friction = 0;
+	private float frictionCoefficient = .2f;
+
+	public float throttleForce;
+
+	private float lastAcceleration;
+
+	public float getMass() { 
+		return mass; 
+	}
+	public void setMass(float mass) {
+		this.mass = mass;
+	}
+
+	public float getVelocity() { 
+		return velocity; 
+	}
+	public float getAcceleration() {
+		lastAcceleration = acceleration;
+		return acceleration;
+	}
+	public float getJerk() {
+		return (acceleration - lastAcceleration) / Time.fixedDeltaTime;
+	}
+	public float getFriction() {
+		return friction;
+	}
+
+	void FixedUpdate() {
+		// as velocity increases, friction value approaches acceleration, thus creating an upper limit for velocity
+		if (velocity != 0) {
+			friction = velocity * frictionCoefficient;
+			//rounding to two digits so it can be printed
+			friction = Mathf.Round((friction * 100f)) / 100f;
+		}
+		else {
+			friction = 0;
+		}
+
+		lastAcceleration = acceleration;
+
+		acceleration = throttleForce - friction;
+
+		velocity += acceleration;
+	}
+
+}
