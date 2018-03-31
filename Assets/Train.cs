532a810686dd81570cebe0434618e6d3d7e9ae31
Zach 🦀🌲 <zachary.jay.crabtree@gmail.com>
2018-03-31 20:13:02 +0200

    Attempt at physics based velocity calculation

--- Assets/Train.cs
+++ Assets/Train.cs
@@ -9,10 +9,10 @@ public class Train : MonoBehaviour {
 	private float velocity = 0;
 	private float acceleration = 0;
 	private float resistance = 0;
-	private float staticFrictionCoefficient = .2f;
+	private float staticFrictionCoefficient = f;
 	private float kineticFrictionCoefficient = .1f;
 	// all constants in drag equation are combined into this single coefficient
-	private float dragCoefficient = 1f;
+	private float dragCoefficient = 3f;
 
 	public float throttleForce;
 
@@ -41,28 +41,48 @@ public class Train : MonoBehaviour {
 	void FixedUpdate () {
 		lastAcceleration = acceleration;
 
-		if (throttleForce != 0 && velocity == 0) {
-			if (Mathf.Abs(throttleForce) < mass * staticFrictionCoefficient) {
-				resistance = throttleForce;
-			} else {
-				resistance = 0;
-			}
-		}
-		else if (velocity > 0.1 || velocity < -0.1) {
-			// add kinetic friction
-			resistance = mass * kineticFrictionCoefficient;
-			// add drag force
-			resistance += .5f * Mathf.Pow(velocity, 2) * dragCoefficient;
+//		float velocityCap = 0 * Math.Pow(throttleForce, 2);
+//
+//		if (throttleForce > 0) {
+//			velocity += .5 * Mathf.Pow(throttleForce, 2);
+//		} else if (throttleForce < 0) {
+//			velocity -= .5 * Mathf.Pow(throttleForce, 2);
+//		}
+//
+//		// want to clamp velocity to cap (proportional to throttleforce)
+//		if (velocity > velocityCap) { velocity = velocityCap; }
+//		// want to clamp velocity to 0 (no going backwards)
+//		if (velocity < 0) { velocity = 0; }
+
+		if (velocity == 0) {
+			resistance = calculateStaticFriction();
 		} else {
-			// round down to 0
-			velocity = 0;
-			resistance = 0;
+			resistance = 1;
 		}
 
 		// if there are more forces added, change to addition of all forces instead of subtraction
 		acceleration = throttleForce - resistance;
 
 		velocity += acceleration;
+	}
+
+	private float calculateStaticFriction() {
+		float maxStaticFriction = mass * staticFrictionCoefficient;
+
+		if (Mathf.Abs(throttleForce) < maxStaticFriction) {
+			return -throttleForce;
+		} else { 
+			return 0;
+		}
+	}
+
+	private float calculateKineticFriction() {
+		return mass * kineticFrictionCoefficient;
+	}
+
+	private float calculateDrag() {
+		return .5f * Mathf.Pow(velocity, 2) * dragCoefficient;
+	
 	}
 
 }
